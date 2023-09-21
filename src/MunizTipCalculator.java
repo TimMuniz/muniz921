bafb9d40ed8ee6203b044ac68526538d733c9d19
BT_1N3_21 <raziz299@nycstudents.net>
2023-09-21 10:07:50 -0400

    beginning

---
+++ src/MunizTipCalculator.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class MunizTipCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the tip calculator");
+        int tablenumber = scan.nextInt();
+
+    }
+}
