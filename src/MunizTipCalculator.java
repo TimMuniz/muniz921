9ee060ef2f709fe5e6b8fc05c7a10c40d31e46d6
BT_1N3_21 <raziz299@nycstudents.net>
2023-09-21 10:16:06 -0400

    on tha grind

--- src/MunizTipCalculator.java
+++ src/MunizTipCalculator.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 public class MunizTipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the tip calculator");
+        System.out.println("Welcome to the tip calculator!");
+        System.out.print("How man people are seated? ");
         int tablenumber = scan.nextInt();
+        System.out.println(tablenumber + " people? Got it.");
+        double billbftip = 0;
 
+        System.out.println("How much tip percent? ");
+        double tippercent = scan.nextDouble();
     }
 }
